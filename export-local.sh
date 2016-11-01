7ef1434d8c17bf5296d7763b871ba8e950e73509
lukasmartinelli <me@lukasmartinelli.ch>
2016-11-01 21:38:55 +0100

    Make more configurable via environment variables

--- export-local.sh
+++ export-local.sh
@@ -9,16 +9,19 @@ readonly RENDER_SCHEME=${RENDER_SCHEME:-pyramid}
 readonly MIN_ZOOM=${MIN_ZOOM:-0}
 readonly MAX_ZOOM=${MAX_ZOOM:-14}
 readonly BBOX=${BBOX:-"-180,-85.0511,180,85.0511"}
+readonly COPY_CONCURRENCY=${COPY_CONCURRENCY:-10}
+readonly TILE_TIMEOUT=${TILE_TIMEOUT:-1800000}
+readonly MBTILES_NAME=${MBTILES_NAME:-tiles.mbtiles}
 
 function export_local_mbtiles() {
-    local mbtiles_name="tiles.mbtiles"
     exec tilelive-copy \
         --scheme=pyramid \
         --bounds="$BBOX" \
-        --timeout=1800000 \
+        --timeout="$TILE_TIMEOUT" \
+        --concurrency="$COPY_CONCURRENCY" \
         --minzoom="$MIN_ZOOM" \
         --maxzoom="$MAX_ZOOM" \
-        "tmsource://$DEST_PROJECT_DIR" "mbtiles://$EXPORT_DIR/$mbtiles_name"
+        "tmsource://$DEST_PROJECT_DIR" "mbtiles://$EXPORT_DIR/$MBTILES_NAME"
 }
 
 function main() {
