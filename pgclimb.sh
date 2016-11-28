499c975345ebaec254d0d260a7f5ae2e4263b534
ImreSamu <ImreSamu@users.noreply.github.com>
2016-11-28 23:11:21 +0100

    add pgclimb.sh

---
+++ pgclimb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#  Start pgclimb command !   
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#echo " Connect PostgreSQL ... "
+max_tries=40
+tries=0
+while ! pg_isready -d $POSTGRES_DB -h $POSTGRES_HOST -U $POSTGRES_USER
+do
+    tries=$((tries + 1))
+    if [ $tries -gt $max_tries ]; then
+        echo "... Gave up , No connections with :  pg_isready -d $POSTGRES_DB -h $POSTGRES_HOST -U $POSTGRES_USER  "
+        exit 1
+    else
+        echo "$(date) - waiting for PG database to start.  Remained tries=$((max_tries - tries))  Host=$POSTGRES_HOST "    
+    fi    
+    sleep 2
+done
+
+pgclimb --password "$POSTGRES_PASSWORD" \
+        --host "$POSTGRES_HOST" \
+        --port "$POSTGRES_PORT" \
+        --dbname="$POSTGRES_DB" \
+        --username="$POSTGRES_USER"  "$@"
