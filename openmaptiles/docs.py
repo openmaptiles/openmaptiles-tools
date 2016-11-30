9629d8fde3f1903fccd946e6a411e20b7af6e13b
Lukas Martinelli <me@lukasmartinelli.ch>
2016-11-30 10:59:21 +0000

    Support specifying values for field openmaptiles/openmaptiles#76

--- openmaptiles/docs.py
+++ openmaptiles/docs.py
@@ -5,6 +5,25 @@ from .tileset import Layer
 import os.path
 
 
+def generate_field_doc(field_name, field_spec):
+    field_doc = "### {name}\n".format(name=field_name.strip())
+
+    if isinstance(field_spec, dict):
+        desc = field_spec.get('description', '')
+        field_doc += "\n" + desc.strip() + "\n"
+
+        values = field_spec.get('values', [])
+        if len(values) > 0:
+            field_doc += "\nPossible values:\n\n"
+            for value in values:
+                field_doc += "- `{value}`\n".format(value=value)
+            field_doc += "\n"
+    else:
+        field_doc += "\n" + field_spec.strip() + "\n\n"
+
+    return field_doc
+
+
 def collect_documentation(layer, diagram=None):
     markdown_doc = "# {layer_id}\n\n{desc}\n\n".format(
         layer_id=layer['layer']['id'].strip(),
@@ -13,11 +32,9 @@ def collect_documentation(layer, diagram=None):
 
     fields_doc = ""
     fields = layer['layer'].get('fields', {}).items()
-    for field_name, field_desc in fields:
-        fields_doc += "- **{name}**: {desc}\n".format(
-            name=field_name.strip(),
-            desc=field_desc.strip()
-        )
+    for field_name, field_spec in fields:
+        fields_doc += generate_field_doc(field_name, field_spec)
+
     if len(fields) > 0:
         markdown_doc += "## Fields\n\n{fields}\n".format(fields=fields_doc)
 
