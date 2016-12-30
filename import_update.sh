746ea24f9379458dc110ba7140c518cb30a05a53
stirringhalo <stirringhalo@users.noreply.github.com>
2016-12-30 15:24:32 -0500

    Add update script.

---
+++ import_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly PG_CONNECT="postgis://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB"
+
+function update() {
+    imposm3 run \
+        -connection "$PG_CONNECT" \
+        -mapping "$MAPPING_YAML" \
+        -cachedir "$IMPOSM_CACHE_DIR"
+}
+
+update
