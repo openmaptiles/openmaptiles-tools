93a0b126f57699a473323d4cf7cae4249599cc81
Lukas Martinelli <me@lukasmartinelli.ch>
2017-01-16 14:23:42 +0100

    Initial commit

---
+++ create_osmborder_lines.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly PG_CONNECT="postgis://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB"
+readonly DIFF_MODE=${DIFF_MODE:-true}
+
+
+function create_borders() {
+    local pbf_file="$1"
+    local filtered_file="$IMPORT_DIR/filtered.osm.pbf"
+    local csv_file="$IMPORT_DIR/osmborder_lines.csv"
+
+    echo "Filter $pbf_file"
+    osmborder_filter -o "$filtered_file" "$pbf_file"
+    echo "Create border lines in $csv_file"
+    osmborder -o "$csv_file" "$filtered_file"
+}
+
+function create_borders_with_first_pbf() {
+    if [ "$(ls -A $IMPORT_DIR/*.pbf 2> /dev/null)" ]; then
+        local pbf_file
+        for pbf_file in "$IMPORT_DIR"/*.pbf; do
+            create_borders "$pbf_file"
+            break
+        done
+    else
+        echo "No PBF files for import found."
+        echo "Please mount the $IMPORT_DIR volume to a folder containing OSM PBF files."
+        exit 404
+    fi
+}
+
+create_borders_with_first_pbf
