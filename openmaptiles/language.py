6aeaccd2f8b1fd07d6d4967b2f0be259a44d978b
jirik <jirik@users.noreply.github.com>
2017-05-09 19:06:18 +0200

    Add name:(non)latin, simplify name_int

--- openmaptiles/language.py
+++ openmaptiles/language.py
@@ -3,6 +3,8 @@ import re
 
 def languages_to_sql(languages):
     name_languages = list(map(lambda l: "NULLIF(tags->'name:"+l+"', '') AS \"name:"+l+"\"", languages))
-    name_languages.append("COALESCE(NULLIF(tags->'int_name', ''), tags->'name') AS name_int");
+    name_languages.append("NULLIF(tags->'name_int', '') AS \"name_int\"");
+    name_languages.append("NULLIF(tags->'name:latin', '') AS \"name:latin\"");
+    name_languages.append("NULLIF(tags->'name:nonlatin', '') AS \"name:nonlatin\"");
     name_languages = ', '.join(name_languages)
     return name_languages
