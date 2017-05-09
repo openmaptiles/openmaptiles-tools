966fb1fbad8bef0df26bafb977d66bca475b7f9d
jirik <jirik@users.noreply.github.com>
2017-05-09 17:40:14 +0200

    Fix name of name_int

--- openmaptiles/language.py
+++ openmaptiles/language.py
@@ -3,6 +3,6 @@ import re
 
 def languages_to_sql(languages):
     name_languages = list(map(lambda l: "NULLIF(tags->'name:"+l+"', '') AS \"name:"+l+"\"", languages))
-    name_languages.append("COALESCE(NULLIF(tags->'int_name', ''), tags->'name')");
+    name_languages.append("COALESCE(NULLIF(tags->'int_name', ''), tags->'name') AS name_int");
     name_languages = ', '.join(name_languages)
     return name_languages
