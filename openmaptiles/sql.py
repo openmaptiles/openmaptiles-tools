6999c5dad4d61ebbf4df0e0021b14531b166f3d0
jirik <jirik@users.noreply.github.com>
2017-05-26 09:39:11 +0200

    Create 'slice_language_tags' function

--- openmaptiles/sql.py
+++ openmaptiles/sql.py
@@ -7,6 +7,11 @@ from .tileset import Tileset
 def collect_sql(tileset_filename):
     tileset = Tileset.parse(tileset_filename)
     sql = ''
+
+    definition = tileset.definition
+    languages = map(lambda l: str(l), definition.get('languages', []))
+    sql += get_slice_language_tags(languages)
+
     for layer in tileset.layers:
         sql += layer_notice(layer['layer']['id'])
         for schema in layer.schemas:
@@ -16,3 +21,18 @@ def collect_sql(tileset_filename):
 
 def layer_notice(layer_name):
     return "DO $$ BEGIN RAISE NOTICE 'Layer {0}'; END$$;".format(layer_name)
+
+SLICE_LANGUAGE_TAGS_SQL="""CREATE OR REPLACE FUNCTION slice_language_tags(tags hstore)
+RETURNS hstore AS $$
+    SELECT slice(tags, ARRAY[{0}])
+$$ LANGUAGE SQL IMMUTABLE;
+"""
+
+def get_slice_language_tags(languages):
+    include_tags = list(map(lambda l: 'name:'+l, languages))
+    include_tags.append('int_name')
+    include_tags.append('name')
+
+    tags_sql = "'" + "', '".join(include_tags) + "'"
+
+    return SLICE_LANGUAGE_TAGS_SQL.format(tags_sql)
