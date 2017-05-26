3617e44de22d5a4b65aa81428653354ed0840f0b
jirik <jirik@users.noreply.github.com>
2017-05-26 13:56:04 +0200

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='openmaptiles-tools',
-    version='0.4.0',
+    version='0.5.0',
     packages=['openmaptiles'],
     license='MIT',
     scripts=[
