773e667b6ce84890d5d52f0ed196e8274548795b
jirik <jirik@users.noreply.github.com>
2017-09-01 15:47:52 +0200

    Move configuration to single file

--- import.py
+++ import.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python
 import psycopg2.extras
 from wikidata import etl, osm
-
-POSTGRES_DB='openmaptiles'
-POSTGRES_USER='openmaptiles'
-POSTGRES_PASSWORD='openmaptiles'
-# POSTGRES_HOST=postgres
-POSTGRES_HOST='localhost'
-POSTGRES_PORT=5432
-
-'''Max number of rows to read from dump'''
-LIMIT = 100000000
-# LIMIT = 100000
-
-'''OSM tables to fetch Wikidata for'''
-OSM_TABLES = ['osm_city_point', 'osm_waterway_linestring']
-
-'''Table with imported wikidata'''
-TABLE_NAME = 'wd_names'
-
-DUMP = 'data/latest-all.json.gz'
+from cfg import *
 
 conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
                         password=POSTGRES_PASSWORD, host=POSTGRES_HOST,
