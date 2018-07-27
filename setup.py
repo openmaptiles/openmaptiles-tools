ecae22d6cf80cd8de80624ca5203f4e6c4dc0ddc
MartinMikita <martin@mikita.eu>
2018-07-27 14:52:46 +0200

    Fixed setup.py - included description, url

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #from distutils.core import setup
 import setuptools
 
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+
 setuptools.setup(
     name='openmaptiles-tools',
     version='0.9.1',
     packages=['openmaptiles'],
+    description="The OpenMapTiles tools for generating TM2Source projects, imposm3 mappings and SQL instructions from OpenMapTiles layers. We encourage other people to use this for their vector tile projects as well since this approach works well for us.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/openmaptiles/openmaptiles-tools",
     license='MIT',
     scripts=[
         'bin/generate-doc',
