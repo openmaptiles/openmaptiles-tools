d76171b0812ff2027744c46bf5bbc30c2fc7c111
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-03-21 05:37:51 -0400

    generate mvt tile from z,x,y

--- openmaptiles/sqltomvt.py
+++ openmaptiles/sqltomvt.py
@@ -7,44 +7,87 @@ from .tileset import Tileset
 from .language import languages_to_sql
 
 
-def generate_sqltomvt(tileset_filename):
-    tileset = Tileset.parse(tileset_filename)
+def generate_sqltomvt_func(opts):
+    header = """
+CREATE OR REPLACE FUNCTION {0}(zoom integer, x integer, y integer)
+RETURNS TABLE(mvt bytea) AS $$
+""".format(opts['fname']).lstrip()
 
-    query_tokens = {
-        'name_languages': languages_to_sql(tileset.definition.get('languages', []))
-    }
+    query = generate_query(opts, "TileBBox(zoom, x, y)", "zoom")
+    footer = ";\n$$ LANGUAGE SQL IMMUTABLE;"
+    return header + query + footer
 
-    extent = 4096
-    queries = [generate_layer(layer, query_tokens, extent) for layer in tileset.layers]
 
-    query = """
+def generate_sqltomvt_preparer(opts):
+    header = """
 DO $$ BEGIN
-IF EXISTS (SELECT * FROM pg_prepared_statements where name = 'gettile') THEN
-  DEALLOCATE gettile;
+IF EXISTS (SELECT * FROM pg_prepared_statements where name = '{0}') THEN
+  DEALLOCATE {0};
 END IF;
 END $$;
 
-PREPARE gettile(geometry, numeric, numeric, numeric) AS
+-- zoom, x, y
+PREPARE {0}(integer, integer, integer) AS
+""".format(opts['fname']).lstrip()
 
-SELECT STRING_AGG(mvtl, '') FROM (
-"""
-    query = query.lstrip() + "  " + "\n    UNION ALL\n  ".join(queries) + "\n) AS ua;"
-
-    return (query
-            .replace("!bbox!", "$1")
-            .replace("!scale_denominator!", "$2")
-            .replace("!pixel_width!", "$3")
-            .replace("!pixel_height!", "$4"))
-
-    # SELECT data, flag
-    # FROM (SELECT data, flag,
-    #              count(*) OVER () AS c
-    # FROM (SELECT ...) AS src
-    # ) AS q
-    # WHERE NOT flag OR c <> 1;
+    return header + generate_query(opts, "TileBBox($1, $2, $3)", "$1") + ";"
 
 
-def generate_layer(layer_def, query_tokens, extent):
+def generate_sqltomvt_raw(opts):
+    return generate_query(opts, None, None, None)
+
+
+def generate_query(opts, bbox, zoom):
+    tileset = Tileset.parse(opts['tileset'])
+    query_tokens = {
+        'name_languages': languages_to_sql(tileset.definition.get('languages', []))
+    }
+    extent = 4096
+    pixel_width = 256
+    pixel_height = 256
+
+    queries = []
+    for layer in tileset.layers:
+        if not opts['rm-empty-layer']:
+            empty_zoom = False
+        elif layer["layer"]['id'] == opts['rm-empty-layer']:
+            empty_zoom = opts['rm-empty-zoom']
+        else:
+            empty_zoom = True
+        queries.append(generate_layer(layer, query_tokens, extent, empty_zoom))
+
+    from_clause = "FROM (\n  " + "\n    UNION ALL\n  ".join(queries) + "\n) AS all_layers"
+
+    if opts['rm-empty-layer']:
+        from_clause = "FROM (SELECT IsEmpty, count(*) OVER () AS LayerCount, mvtl " + from_clause + ") AS counter_layers"
+
+    query = "SELECT STRING_AGG(mvtl, '') " + from_clause
+
+    if opts['rm-empty-layer']:
+        query += " WHERE NOT IsEmpty OR LayerCount <> 1"
+
+    if bbox is None:
+        return query
+
+    query = (query
+             .replace("!bbox!", bbox)
+             .replace("z(!scale_denominator!)", zoom)
+             .replace("!pixel_width!", str(pixel_width))
+             .replace("!pixel_height!", str(pixel_height)))
+
+    if '!scale_denominator!' in query:
+        raise ValueError('We made invalid assumption that "!scale_denominator!" is always used '
+                         'as a parameter to z() function. Either change the layer queries, or fix this code')
+
+    return query
+
+
+def generate_layer(layer_def, query_tokens, extent, empty_zoom):
+    """
+    If empty_zoom is True, adds an extra sql column with a constant value,
+    otherwise if it is an integer, tests if the geometry of this layer covers the whole tile, and outputs true/false,
+    otherwise no extra column is added
+    """
     layer = layer_def["layer"]
     buffer = layer['buffer_size']
     query = layer["datasource"]["query"].format(**query_tokens)
@@ -58,13 +101,26 @@ def generate_layer(layer_def, query_tokens, extent):
         "geometry",
         "ST_AsMVTGeom(geometry, !bbox!, {extent}, {buffer}, true) AS mvtgeometry".format(extent=extent, buffer=buffer))
 
+    if empty_zoom == False:
+        filter = ""
+    elif empty_zoom == True:
+        filter = "FALSE AS IsEmpty, "
+    else:
+        # Test that geometry covers the whole tile
+        wkt_polygon = "MULTIPOLYGON((({0} {1},{0} {0},{1} {0},{1} {1},{0} {1})))".format(-buffer, extent + buffer)
+        filter = ("CASE z(!scale_denominator!) < {0} "
+                  "WHEN TRUE THEN FALSE "
+                  "ELSE ST_EQUALS(ST_COLLECT(mvtgeometry), ST_GeomFromText('{1}', 3857)) "
+                  "END AS IsEmpty, ".format(empty_zoom, wkt_polygon))
+
     query = (
-        # Combine all layer's features into a single MVT blob representing one layer
-        "SELECT ST_AsMVT(tile, '{id}', {extent}, 'mvtgeometry') as mvtl "
-        # only if the MVT geometry is not NULL
-        "FROM ({query} WHERE ST_AsMVTGeom(geometry, !bbox!, {extent}, {buffer}, true) IS NOT NULL) AS tile "
-        # Skip the whole layer if there is nothing in it
-        "HAVING COUNT(*) > 0"
+            "SELECT " + filter +
+            # Combine all layer's features into a single MVT blob representing one layer
+            "ST_AsMVT(tile, '{id}', {extent}, 'mvtgeometry') as mvtl "
+            # only if the MVT geometry is not NULL
+            "FROM ({query} WHERE ST_AsMVTGeom(geometry, !bbox!, {extent}, {buffer}, true) IS NOT NULL) AS tile "
+            # Skip the whole layer if there is nothing in it
+            "HAVING COUNT(*) > 0"
     ).format(id=layer['id'], extent=extent, query=query, buffer=buffer)
 
     return query
