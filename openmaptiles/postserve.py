8a4b3bcb6a9c756e1618dca46772443f54e5445f
Yuri Astrakhan <yuriastrakhan@gmail.com>
2019-10-27 14:28:39 -0400

    Postserve cleanups, readme updates (#69)

--- openmaptiles/postserve.py
+++ openmaptiles/postserve.py
@@ -1,10 +1,10 @@
-import asyncpg
 import logging
-import io
+from functools import partial
+
+import asyncpg
 import tornado.ioloop
 import tornado.web
-from datetime import datetime
-from functools import partial
+from asyncpg.pool import Pool
 
 from openmaptiles.consts import PIXEL_SCALE
 from openmaptiles.language import languages_to_sql
@@ -12,7 +12,6 @@ from openmaptiles.sqltomvt import generate_sqltomvt_query
 from openmaptiles.tileset import Tileset
 
 
-# noinspection PyAbstractClass
 class RequestHandledWithCors(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -25,11 +24,14 @@ class RequestHandledWithCors(tornado.web.RequestHandler):
 
 
 class GetTile(RequestHandledWithCors):
+    pool: Pool
+    query: str
+
     def initialize(self, pool, query):
         self.pool = pool
         self.query = query
 
-    async def get(self, zoom,x,y):
+    async def get(self, zoom, x, y):
         self.set_header("Content-Type", "application/x-protobuf")
         self.set_header("Content-Disposition", "attachment")
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -38,10 +40,9 @@ class GetTile(RequestHandledWithCors):
             self.write(tile)
 
 
-# noinspection PyAbstractClass
 class GetMetadata(RequestHandledWithCors):
+    metadata: str
 
-    # noinspection PyAttributeOutsideInit
     def initialize(self, metadata):
         self.metadata = metadata
 
@@ -49,26 +50,30 @@ class GetMetadata(RequestHandledWithCors):
         self.write(self.metadata)
         print('Returning metadata')
 
+
 async def generate_metadata(pool, tileset, host, port, metadata):
     async with pool.acquire() as connection:
-        # Get all Postgres types and keep those we know about (could be optimized further)
+        # Get all Postgres types and keep those we know about
         known_types = dict(bool="Boolean", text="String", int4="Number", int8="Number")
         types = await connection.fetch("select oid, typname from pg_type")
-        pg_types = {row[0]: known_types[row[1]] for row in types if row[1] in known_types}
+        pg_types = {row[0]: known_types[row[1]] for row in types if
+                    row[1] in known_types}
 
         vector_layers = []
         for layer_def in tileset.layers:
             layer = layer_def["layer"]
 
             # Get field names and types by executing a dummy query
+            langs = languages_to_sql(tileset.definition.get('languages', []))
             query = (layer['datasource']['query']
-                     .format(name_languages=languages_to_sql(tileset.definition.get('languages', [])))
+                     .format(name_languages=langs)
                      .replace("!bbox!", "TileBBox(0, 0, 0)")
                      .replace("z(!scale_denominator!)", "0")
                      .replace("!pixel_width!", str(PIXEL_SCALE))
                      .replace("!pixel_height!", str(PIXEL_SCALE)))
             st = await connection.prepare(f"SELECT * FROM {query} WHERE false LIMIT 0")
-            fields = {fld.name: pg_types[fld.type.oid] for fld in st.get_attributes() if fld.type.oid in pg_types}
+            fields = {fld.name: pg_types[fld.type.oid]
+                      for fld in st.get_attributes() if fld.type.oid in pg_types}
 
             vector_layers.append(dict(
                 id=layer["id"],
@@ -81,8 +86,9 @@ async def generate_metadata(pool, tileset, host, port, metadata):
         metadata["vector_layers"] = vector_layers
         metadata["tiles"] = [f"http://{host}:{port}" + "/tiles/{z}/{x}/{y}.pbf"]
 
-def serve(host, port, pghost, pgport, dbname, user, password, metadata, tileset_path, sql_file, mask_layer, mask_zoom,
-          verbose):
+
+def serve(host, port, pghost, pgport, dbname, user, password, metadata, tileset_path,
+          sql_file, mask_layer, mask_zoom, verbose):
     tileset = Tileset.parse(tileset_path)
 
     if sql_file:
@@ -101,11 +107,13 @@ def serve(host, port, pghost, pgport, dbname, user, password, metadata, tileset_
 
     tornado.log.access_log.setLevel(logging.INFO if verbose else logging.ERROR)
 
-    dsn = 'postgresql://'+user+':'+password+'@'+pghost+':'+pgport+'/'+dbname
+    dsn = f"postgresql://{user}:{password}@{pghost}:{pgport}/{dbname}"
 
     io_loop = tornado.ioloop.IOLoop.current()
     pool = io_loop.run_sync(partial(asyncpg.create_pool, dsn=dsn))
-    io_loop.run_sync(partial(generate_metadata, pool=pool, tileset=tileset, host=host, port=port, metadata=metadata))
+    io_loop.run_sync(
+        partial(generate_metadata, pool=pool, tileset=tileset, host=host, port=port,
+                metadata=metadata))
 
     application = tornado.web.Application([
         (
