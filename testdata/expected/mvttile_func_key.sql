8548e3161cba56e77c9afce9277e5f8158085927
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-12-05 19:08:38 -0500

    sqltomvt drop function instead of replacing
    replace does not work if the function return type changes.

--- testdata/expected/mvttile_func_key.sql
+++ testdata/expected/mvttile_func_key.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION gettile(zoom integer, x integer, y integer)
+DROP FUNCTION IF EXISTS gettile(integer, integer, integer);
+CREATE FUNCTION gettile(zoom integer, x integer, y integer)
 RETURNS TABLE(mvt bytea, key text) AS $$
 SELECT mvt, md5(mvt) AS key FROM (SELECT STRING_AGG(mvtl, '') AS mvt FROM (
   SELECT COALESCE(ST_AsMVT(t, 'housenumber', 4096, 'mvtgeometry'), '') as mvtl FROM (SELECT ST_AsMVTGeom(geometry, ST_TileEnvelope(zoom, x, y), 4096, 8, true) AS mvtgeometry, housenumber, NULLIF(tags->'name:en', '') AS "name:en", NULLIF(tags->'name:de', '') AS "name:de", NULLIF(tags->'name:cs', '') AS "name:cs", NULLIF(tags->'name_int', '') AS "name_int", NULLIF(tags->'name:latin', '') AS "name:latin", NULLIF(tags->'name:nonlatin', '') AS "name:nonlatin" FROM layer_housenumber(ST_TileEnvelope(zoom, x, y), zoom)) AS t
