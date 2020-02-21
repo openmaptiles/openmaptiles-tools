54c0134ac3e2df7364fa8491dc2424dac2aa610f
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-02-21 18:34:50 -0500

    rewrote tags/include logic

--- openmaptiles/imposm.py
+++ openmaptiles/imposm.py
@@ -25,13 +25,9 @@ def create_imposm3_mapping(tileset_filename):
     include_tags.append('name')
     include_tags.append('wikidata')
     include_tags.append('wikipedia')
-    include_tags = set(include_tags)
 
     generalized_tables = {}
     tables = {}
-    tags = {
-        'include': include_tags
-    }
 
     for layer in tileset.layers:
         for mapping in layer.imposm_mappings:
@@ -39,12 +35,14 @@ def create_imposm3_mapping(tileset_filename):
                 if 'tolerance' in definition:
                     try:  # Test if numeric
                         float(definition['tolerance'])
-                    except:
+                    except ValueError:
                         if re.match(r"^ZRES\d{1,2}$", definition['tolerance']):
                             zoom = definition['tolerance'][4:6]
-                            definition['tolerance'] = zres(float(pixel_scale), float(zoom))  # Convert to distance
+                            definition['tolerance'] = zres(float(pixel_scale), float(
+                                zoom))  # Convert to distance
                         else:
-                            raise SyntaxError('Unrecognized tolerance ' + str(definition['tolerance']))
+                            raise SyntaxError('Unrecognized tolerance ' + str(
+                                definition['tolerance']))
                 if 'sql_filter' in definition:
                     definition['sql_filter'] = re.sub(
                         r"ZRES\d{1,2}",
@@ -55,15 +53,20 @@ def create_imposm3_mapping(tileset_filename):
                 # Remove all OpenMapTiles custom keys to avoid confusing Imposm
                 definition.pop('_resolve_wikidata', None)
                 tables[table_name] = definition
-            for tag_name, definition in mapping.get('tags', {}).items():
-                if tag_name == 'include':
-                    tags[tag_name] |= set(definition)
-                tags[tag_name] = definition
-
-    include_tags = sorted(list(include_tags))
+            for tags_key, tags_val in mapping.get('tags', {}).items():
+                if tags_key == 'include':
+                    if not isinstance(tags_val, list) or any(
+                        (v for v in tags_val if not v or not isinstance(v, str))
+                    ):
+                        raise ValueError(f"Tileset {tileset.name} mapping's "
+                                         f"tags/include must be a list of strings")
+                    include_tags += tags_val
+                else:
+                    raise ValueError(f"Tileset {tileset.name} mapping tags "
+                                     f"uses an unsupported key '{tags_key}'")
 
     return {
-        'tags': tags,
+        'tags': dict(include=list(sorted(set(include_tags)))),
         'generalized_tables': generalized_tables,
         'tables': tables,
     }
