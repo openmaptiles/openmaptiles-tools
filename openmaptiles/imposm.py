02f15e4bdf4e68b66fd5a61d645b3754945d6e9e
Yuri Astrakhan <yuriastrakhan@gmail.com>
2020-02-21 18:45:15 -0500

    Imposm tags (#181)
    Fix imposm tags handling, allowing different layers to include additional tags (rather than overriding it, thus the last layer wins).
    
    Fixes #179
    
    Side cleanup - slightly better syntax for zres parsing & error handling.
    
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>

--- openmaptiles/imposm.py
+++ openmaptiles/imposm.py
@@ -4,12 +4,12 @@ from .tileset import Tileset
 
 def zres(pixel_scale, zoom):
     # See https://github.com/openmaptiles/postgis-vt-util/blob/master/src/ZRes.sql
-    return 40075016.6855785 / ((1.0 * pixel_scale) * 2 ** zoom)
+    return 40075016.6855785 / ((1.0 * float(pixel_scale)) * 2 ** float(zoom))
 
 
 def call_zres(pixel_scale, match):
     # See https://github.com/openmaptiles/postgis-vt-util/blob/master/src/ZRes.sql
-    return str(zres(float(pixel_scale), float(match.group(0)[4:6])))
+    return str(zres(pixel_scale, match.group(0)[4:6]))
 
 
 def create_imposm3_mapping(tileset_filename):
@@ -28,9 +28,6 @@ def create_imposm3_mapping(tileset_filename):
 
     generalized_tables = {}
     tables = {}
-    tags = {
-        'include': include_tags
-    }
 
     for layer in tileset.layers:
         for mapping in layer.imposm_mappings:
@@ -38,12 +35,14 @@ def create_imposm3_mapping(tileset_filename):
                 if 'tolerance' in definition:
                     try:  # Test if numeric
                         float(definition['tolerance'])
-                    except:
+                    except ValueError:
                         if re.match(r"^ZRES\d{1,2}$", definition['tolerance']):
                             zoom = definition['tolerance'][4:6]
-                            definition['tolerance'] = zres(float(pixel_scale), float(zoom))  # Convert to distance
+                            # Convert to distance
+                            definition['tolerance'] = zres(pixel_scale, zoom)
                         else:
-                            raise SyntaxError('Unrecognized tolerance ' + str(definition['tolerance']))
+                            raise SyntaxError(
+                                f"Unrecognized tolerance '{definition['tolerance']}'")
                 if 'sql_filter' in definition:
                     definition['sql_filter'] = re.sub(
                         r"ZRES\d{1,2}",
@@ -54,11 +53,20 @@ def create_imposm3_mapping(tileset_filename):
                 # Remove all OpenMapTiles custom keys to avoid confusing Imposm
                 definition.pop('_resolve_wikidata', None)
                 tables[table_name] = definition
-            for tag_name, definition in mapping.get('tags', {}).items():
-                tags[tag_name] = definition
+            for tags_key, tags_val in mapping.get('tags', {}).items():
+                if tags_key == 'include':
+                    if not isinstance(tags_val, list) or any(
+                        (v for v in tags_val if not v or not isinstance(v, str))
+                    ):
+                        raise ValueError(f"Tileset {tileset.name} mapping's "
+                                         f"tags/include must be a list of strings")
+                    include_tags += tags_val
+                else:
+                    raise ValueError(f"Tileset {tileset.name} mapping tags "
+                                     f"uses an unsupported key '{tags_key}'")
 
     return {
-        'tags': tags,
+        'tags': dict(include=list(sorted(set(include_tags)))),
         'generalized_tables': generalized_tables,
         'tables': tables,
     }
