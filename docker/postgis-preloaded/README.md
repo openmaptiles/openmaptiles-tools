a8fe98426b9a4ef11b0e58125e9790e8d939dde4
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-03-07 12:54:00 -0500

    New postgis-preloaded image - db with base data
    This is a Docker image that has been pre-loaded with lakelines, natural-earth, and water data.
    This image can be used for quicker testing, but might be less suited for a production environment.
    
    The image contains a PostgreSQL database `openmaptiles`, user=openmaptiles, password=openmaptiles

---
+++ docker/postgis-preloaded/README.md
@@ -0,0 +1,6 @@
+# Preloaded Postgis Image [![Docker Automated build](https://img.shields.io/docker/automated/openmaptiles/postgis-preloaded.svg?maxAge=2592000)](https://hub.docker.com/r/openmaptiles/postgis-preloaded/)
+
+This is a Docker image that has been pre-loaded with lakelines, natural-earth, and water data.
+This image can be used for quicker testing, but might be less suited for a production environment.
+
+The image contains a PostgreSQL database `openmaptiles`, user=openmaptiles, password=openmaptiles
---
+++ docker/postgis-preloaded/custom-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+# This code was adapted from a postgres issue
+# https://github.com/docker-library/postgres/issues/661#issuecomment-573192715
+
+source "$(command -v docker-entrypoint.sh)"
+
+docker_setup_env
+docker_create_db_directories
+
+if [[ -z "$DATABASE_ALREADY_EXISTS" ]]; then
+  docker_verify_minimum_env
+  docker_init_database_dir
+  pg_setup_hba_conf
+
+  # only required for '--auth[-local]=md5' on POSTGRES_INITDB_ARGS
+  export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+
+  # Support for the import scripts from other docker containers
+  # Override PGCONN so that ogr2ogr would connect via socket rather than TCP
+  export PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER password=$PGPASSWORD"
+
+  docker_temp_server_start "$@" -c max_locks_per_transaction=256
+  docker_setup_db
+  docker_process_init_files /docker-entrypoint-initdb.d/*
+  docker_temp_server_stop
+fi
+
+exec postgres "$@"
