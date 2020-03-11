30205a480ff9d92a9484850c0ab0370d3d5f1273
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-03-11 19:15:35 -0400

    Upgrade postgis image to be based on official Postgis:11

--- docker/postgis/initdb-postgis.sh
+++ docker/postgis/initdb-postgis.sh
@@ -3,26 +3,18 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "+++++++++++++++++++++++++++"
-echo "  Create template_postgis"
-echo "+++++++++++++++++++++++++++"
-
-# TODO: explain why this is needed
-PGUSER="$POSTGRES_USER" psql --dbname="$POSTGRES_DB" <<-'EOSQL'
-    CREATE DATABASE template_postgis;
-    UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
-EOSQL
-
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "  Create extensions in template_postgis and $POSTGRES_DB DBs"
+echo "  Loading OMT postgis extensions"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 for db in template_postgis "$POSTGRES_DB"; do
+echo "Loading extensions into $db"
 PGUSER="$POSTGRES_USER" psql --dbname="$db" <<-'EOSQL'
-    CREATE EXTENSION postgis;
+    -- CREATE EXTENSION postgis; -- already loaded by parent docker
+    -- CREATE EXTENSION fuzzystrmatch; -- already loaded by parent docker
     CREATE EXTENSION hstore;
     CREATE EXTENSION unaccent;
-    CREATE EXTENSION fuzzystrmatch;
     CREATE EXTENSION osml10n;
+    CREATE EXTENSION gzip;
 EOSQL
 done
