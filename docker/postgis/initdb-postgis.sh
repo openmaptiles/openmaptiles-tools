121797cc7ff905840a195a753d56c0a2b26908d6
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-03-18 18:18:04 -0400

    Bump utf8proc and german_l10n libs, rm unused ext

--- docker/postgis/initdb-postgis.sh
+++ docker/postgis/initdb-postgis.sh
@@ -10,11 +10,18 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 for db in template_postgis "$POSTGRES_DB"; do
 echo "Loading extensions into $db"
 PGUSER="$POSTGRES_USER" psql --dbname="$db" <<-'EOSQL'
-    -- CREATE EXTENSION postgis; -- already loaded by parent docker
-    -- CREATE EXTENSION fuzzystrmatch; -- already loaded by parent docker
-    CREATE EXTENSION hstore;
-    CREATE EXTENSION unaccent;
-    CREATE EXTENSION osml10n;
-    CREATE EXTENSION gzip;
+    -- Cleanup. Ideally parent container shouldn't pre-install those.
+    DROP EXTENSION IF EXISTS postgis_tiger_geocoder;
+    DROP EXTENSION IF EXISTS postgis_topology;
+
+    -- These extensions are already loaded by the parent docker
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+
+    -- Extensions needed for OpenMapTiles
+    CREATE EXTENSION IF NOT EXISTS hstore;
+    CREATE EXTENSION IF NOT EXISTS unaccent;
+    CREATE EXTENSION IF NOT EXISTS osml10n;
+    CREATE EXTENSION IF NOT EXISTS gzip;
 EOSQL
 done
