35843aaa4856597daffe51ba59b85195e57b7ea2
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-05-17 23:24:16 -0400

    downloader autodetection

--- tests/test-tools.sh
+++ tests/test-tools.sh
@@ -50,7 +50,7 @@ generate-mapping-graph "$TESTLAYERS/housenumber/housenumber.yaml" "$DEVDOC/mappi
 
 # Using a fake cache/geofabrik.json so that downloader wouldn't need to download the real one from Geofabrik site
 download-osm planet --imposm-cfg "$BUILD/planet-cfg.json" --dry-run
-download-osm geofabrik michigan --imposm-cfg "$BUILD/michigan-cfg.json" --dry-run
+download-osm michigan --imposm-cfg "$BUILD/michigan-cfg.json" --dry-run
 
 
 # Run background http server, and stop it when this script exits
