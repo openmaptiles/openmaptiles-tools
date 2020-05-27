openmaptiles/openmaptiles-tools

commit bb072fee3725f4967bc68922bed19cb8442d3cad

SQL importing and env var improvements (#234)
### environment variable cleanup

Long overdue cleanup of the environment variables to clarify their meaning.  Ideally we should do this as part of import-osm -> tools migration because it requires people to update their docker image usage anyway, so would be good to modify it at the same time.

* rename `CONFIG_JSON` ⇒ `IMPOSM_CONFIG_FILE`
* rename `MAPPING_YAML` ⇒ `IMPOSM_MAPPING_FILE`
* rename `IMPORT_DIR` ⇒ `PBF_DATA_DIR`
* rename `DIFF_DIR` ⇒ `IMPOSM_DIFF_DIR`
* rename `TILES_DIR` ⇒ `EXPIRETILES_DIR`

* rename `OMT_UTIL_DIR` and `VT_UTIL_DIR` ⇒ `SQL_TOOLS_DIR` (merged)

### related improvements
* Combine language and utility SQL files into a single dir - `/sql`, which means both `VT_UTIL_DIR` and `OMT_UTIL_DIR` have been removed, and a single `SQL_TOOLS_DIR` location was added.
* `import-osm` can now accept PBF file as the first parameter, rather than auto-search for the first file in the `PBF_DATA_DIR`

A minor side change -- validate that the variables are set and abort early

Dockerfile
@@ -71,21 +71,19 @@ ARG TOOLS_DIR=/usr/src/app
 
 WORKDIR ${TOOLS_DIR}
 
-# Using VT_UTIL_DIR and OMT_UTIL_DIR vars allow users to provide custom util files:
-# postgis-vt-util.sql and language.sql
-# See README
-ENV VT_UTIL_DIR=/opt/postgis-vt-util \
-    OMT_UTIL_DIR="${TOOLS_DIR}/sql" \
-    TOOLS_DIR="$TOOLS_DIR" \
-    SQL_DIR=/sql \
+#
+# IMPOSM_CONFIG_FILE can be used to provide custom IMPOSM config file
+# SQL_TOOLS_DIR can be used to provide custom SQL files instead of vt_utils and other files from /sql
+#
+ENV TOOLS_DIR="$TOOLS_DIR" \
     PATH="${TOOLS_DIR}:${PATH}" \
-    IMPORT_DIR=/import \
+    IMPOSM_CONFIG_FILE=${TOOLS_DIR}/config/repl_config.json \
+    IMPOSM_MAPPING_FILE=/mapping/mapping.yaml \
     IMPOSM_CACHE_DIR=/cache \
-    MAPPING_YAML=/mapping/mapping.yaml \
-    DIFF_DIR=/import \
-    TILES_DIR=/import \
-    CONFIG_JSON=${TOOLS_DIR}/config/repl_config.json
-
+    IMPOSM_DIFF_DIR=/import \
+    PBF_DATA_DIR=/import \
+    SQL_DIR=/sql \
+    SQL_TOOLS_DIR="${TOOLS_DIR}/sql"
 
 
 RUN set -eux ;\
@@ -124,8 +122,8 @@ COPY ./requirements.txt .
 
 RUN set -eux ;\
     pip install --no-cache-dir -r requirements.txt ;\
-    mkdir -p "${VT_UTIL_DIR?}" ;\
-    wget --quiet --progress=bar:force:noscroll --show-progress -O "${VT_UTIL_DIR}/10_postgis-vt-util.sql" \
+    mkdir -p "${SQL_TOOLS_DIR:?}" ;\
+    wget --quiet --progress=bar:force:noscroll --show-progress -O "${SQL_TOOLS_DIR}/10_postgis-vt-util.sql" \
        https://raw.githubusercontent.com/openmaptiles/postgis-vt-util/${VT_UTIL_VERSION}/postgis-vt-util.sql
 
 # Copy tools, imposm, and osmborder into the app dir

docker/import-data/Dockerfile
@@ -30,9 +30,9 @@ RUN DIR=/downloads/lake_centerline \
 FROM osgeo/gdal:alpine-normal-3.0.2
 LABEL maintainer="Yuri Astrakhan <YuriAstrakhan@gmail.com>"
 
-ENV IMPORT_DIR=/import
+ENV DATA_DIR=/import
 
-COPY --from=downloader /downloads $IMPORT_DIR/
+COPY --from=downloader /downloads $DATA_DIR/
 
 WORKDIR /usr/src/app
 COPY import_data.sh /usr/src/app

docker/postgis-preloaded/Dockerfile
@@ -19,7 +19,7 @@ FROM openmaptiles/postgis:${OMT_TOOLS_VERSION} as builder
 # See also a relevant discussion about data pre-packaging in
 # https://github.com/docker-library/postgres/issues/661#issuecomment-573192715
 
-ENV IMPORT_DIR=/import \
+ENV DATA_DIR=/import \
     POSTGRES_DB=openmaptiles \
     POSTGRES_USER=openmaptiles \
     POSTGRES_PASSWORD=openmaptiles
@@ -33,7 +33,7 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update \
 # switch to postgres user for ownership and execution
 USER postgres
 
-COPY --from=data $IMPORT_DIR $IMPORT_DIR/
+COPY --from=data $DATA_DIR $DATA_DIR/
 
 # Postgis adds 10_... script, and OMT postgis adds 20_... script, so run after both are done
 COPY --from=data /usr/src/app/import_data.sh /docker-entrypoint-initdb.d/30_omt_preload.sh

bin/import-borders
@@ -3,22 +3,22 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-: "${IMPORT_DIR:=/import}"
+: "${PBF_DATA_DIR:=/import}"
 # Do not use .pbf extension to avoid accidental conflict with the original pbf with multiple runs
-: "${BORDERS_PBF_FILE:="$IMPORT_DIR/borders/filtered.pbf"}"
-: "${BORDERS_CSV_FILE:="$IMPORT_DIR/borders/lines.csv"}"
+: "${BORDERS_PBF_FILE:="$PBF_DATA_DIR/borders/filtered.pbf"}"
+: "${BORDERS_CSV_FILE:="$PBF_DATA_DIR/borders/lines.csv"}"
 : "${BORDERS_TABLE_NAME:=osm_border_linestring}"
 
 function usage() {
   >&2 echo "Usage"
   >&2 echo ""
-  >&2 echo "import-borders                      - Parse and import first PBF file in IMPORT_DIR"
+  >&2 echo "import-borders                      - Parse and import first PBF file in PBF_DATA_DIR"
   >&2 echo "import-borders [import] planet.pbf  - Parse and import planet.pbf"
   >&2 echo "import-borders parse planet.pbf     - Parse planet.pbf into a CSV file, but do not import"
   >&2 echo "import-borders load borders.csv     - Load borders.csv into a table"
   >&2 echo ""
   >&2 echo "Expected variables:  PGHOST, PGDATABASE, PGUSER, PGPASSWORD"
-  >&2 echo "Supported variables:  IMPORT_DIR, BORDERS_PBF_FILE, BORDERS_CSV_FILE, BORDERS_TABLE_NAME, PGPORT"
+  >&2 echo "Supported variables:  PBF_DATA_DIR, BORDERS_PBF_FILE, BORDERS_CSV_FILE, BORDERS_TABLE_NAME, PGPORT"
   >&2 echo ""
   >&2 echo "If first parameter is a file, treat it as 'import'"
   >&2 echo ""
@@ -26,15 +26,15 @@ function usage() {
 }
 
 # First argument is the PBF file to import.
-# If there are no arguments, searches for the first *.pbf file in the $IMPORT_DIR
+# If there are no arguments, searches for the first *.pbf file in the $PBF_DATA_DIR
 if [[ $# -eq 0 ]]; then
-  if [ "$(ls -A "$IMPORT_DIR"/*.pbf 2> /dev/null)" ]; then
-    for pbf_file in "$IMPORT_DIR"/*.pbf; do
+  if [ "$(ls -A "$PBF_DATA_DIR"/*.pbf 2> /dev/null)" ]; then
+    for pbf_file in "$PBF_DATA_DIR"/*.pbf; do
       set -- import "$pbf_file"
       break
     done
   else
-    echo "No PBF files found in the $IMPORT_DIR dir."
+    echo "No PBF files found in the $PBF_DATA_DIR dir."
     usage
   fi
 elif [[ "$1" != "import" && "$1" != "load" && "$1" != "parse" ]]; then

bin/import-diff
@@ -11,18 +11,13 @@ export PGDATABASE="${POSTGRES_DB:-${PGDATABASE?}}"
 export PGUSER="${POSTGRES_USER:-${PGUSER?}}"
 export PGPASSWORD="${POSTGRES_PASSWORD:-${PGPASSWORD?}}"
 export PGPORT="${POSTGRES_PORT:-${PGPORT:-5432}}"
-export EXPIRETILES_ZOOM="${EXPIRETILES_ZOOM:-14}"
 
-function import_diff() {
-    imposm diff \
-        -connection "postgis://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE" \
-        -mapping "$MAPPING_YAML" \
-        -cachedir "$IMPOSM_CACHE_DIR" \
-        -diffdir "$IMPORT_DIR" \
-        -expiretiles-dir "$IMPORT_DIR" \
-        -expiretiles-zoom "$EXPIRETILES_ZOOM" \
-        -config "$CONFIG_JSON" \
-        "$IMPORT_DIR/changes.osc.gz"
-}
-
-import_diff
+imposm diff \
+    -connection "postgis://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE" \
+    -mapping "${IMPOSM_MAPPING_FILE:?}" \
+    -cachedir "${IMPOSM_CACHE_DIR:?}" \
+    -diffdir "${IMPOSM_DIFF_DIR:?}" \
+    -expiretiles-dir "${EXPIRETILES_DIR:?}" \
+    -expiretiles-zoom "${EXPIRETILES_ZOOM:-14}" \
+    -config "${IMPOSM_CONFIG_FILE:?}" \
+    "${PBF_DATA_DIR:?}/changes.osc.gz"

bin/import-osm
@@ -28,28 +28,37 @@ function import_pbf() {
 
     imposm import \
         -connection "postgis://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE" \
-        -mapping "$MAPPING_YAML" \
+        -mapping "${IMPOSM_MAPPING_FILE:?}" \
         -overwritecache \
-        -diffdir "$DIFF_DIR" \
-        -cachedir "$IMPOSM_CACHE_DIR" \
+        -diffdir "${IMPOSM_DIFF_DIR:?}" \
+        -cachedir "${IMPOSM_CACHE_DIR:?}" \
         -read "$pbf_file" \
         -deployproduction \
-        -write $diff_flag \
-        -config "$CONFIG_JSON"
+        -write \
+        $diff_flag \
+        -config "${IMPOSM_CONFIG_FILE:?}"
 }
 
 function import_osm_with_first_pbf() {
-    if [ "$(ls -A "$IMPORT_DIR"/*.pbf 2> /dev/null)" ]; then
-        local pbf_file
-        for pbf_file in "$IMPORT_DIR"/*.pbf; do
-            import_pbf "$pbf_file"
-            break
-        done
+    if [[ -z "${1:-}" ]]; then
+      # With no parameters, import the first found PBF file in the $PBF_DATA_DIR
+      echo "Searching for any *.pbf files in $PBF_DATA_DIR"
+      if [ "$(ls -A "${PBF_DATA_DIR:?}"/*.pbf 2> /dev/null)" ]; then
+          local pbf_file
+          for pbf_file in "$PBF_DATA_DIR"/*.pbf; do
+              import_pbf "$pbf_file"
+              break
+          done
+      else
+          echo "No PBF files for import found."
+          echo "Please mount the $PBF_DATA_DIR volume to a folder containing OSM PBF files,"
+          echo "or provide PBF file as the first parameter to import-osm"
+          exit 1
+      fi
     else
-        echo "No PBF files for import found."
-        echo "Please mount the $IMPORT_DIR volume to a folder containing OSM PBF files."
-        exit 1
+      echo "Importing provided file $1"
+      import_pbf "$1"
     fi
 }
 
-import_osm_with_first_pbf
+import_osm_with_first_pbf "${1:-}"

bin/import-sql
@@ -65,12 +65,8 @@ function import_all_sql_files() {
 # If there are no arguments, imports everything,
 # otherwise the first argument is the name of a file to be imported.
 if [[ $# -eq 0 ]]; then
-  if [[ "${OMT_UTIL_DIR:-}" == "" ]]; then
-    echo "ENV variable OMT_UTIL_DIR is not set. It should contain directory with .sql files, e.g. language.sql "
-    exit 1
-  fi
-  if [[ "${VT_UTIL_DIR:-}" == "" ]]; then
-    echo "ENV variable VT_UTIL_DIR is not set. It should contain directory with .sql files, e.g. postgis-vt-util.sql"
+  if [[ "${SQL_TOOLS_DIR:-}" == "" ]]; then
+    echo "ENV variable SQL_TOOLS_DIR is not set. It should contain directory with .sql files, e.g. postgis-vt-util.sql and language.sql"
     exit 1
   fi
   if [[ "${SQL_DIR:-}" == "" ]]; then
@@ -78,8 +74,7 @@ if [[ $# -eq 0 ]]; then
     exit 1
   fi
 
-  import_all_sql_files "$VT_UTIL_DIR"  # import postgis-vt-util.sql
-  import_all_sql_files "$OMT_UTIL_DIR"  # import language.sql
+  import_all_sql_files "$SQL_TOOLS_DIR"  # import postgis-vt-util.sql and all other tools SQL files from /sql
   import_all_sql_files "$SQL_DIR"  # import compiled tileset.sql
 else
   exec_psql_file "$1"

bin/import-update
@@ -11,17 +11,12 @@ export PGDATABASE="${POSTGRES_DB:-${PGDATABASE?}}"
 export PGUSER="${POSTGRES_USER:-${PGUSER?}}"
 export PGPASSWORD="${POSTGRES_PASSWORD:-${PGPASSWORD?}}"
 export PGPORT="${POSTGRES_PORT:-${PGPORT:-5432}}"
-export EXPIRETILES_ZOOM="${EXPIRETILES_ZOOM:-14}"
 
-function update() {
-    imposm run \
-        -connection "postgis://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE" \
-        -mapping "$MAPPING_YAML" \
-        -cachedir "$IMPOSM_CACHE_DIR" \
-        -diffdir "$DIFF_DIR" \
-        -expiretiles-dir "$TILES_DIR" \
-        -expiretiles-zoom "$EXPIRETILES_ZOOM" \
-        -config "$CONFIG_JSON"
-}
-
-update
+imposm run \
+    -connection "postgis://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE" \
+    -mapping "${IMPOSM_MAPPING_FILE:?}" \
+    -cachedir "${IMPOSM_CACHE_DIR:?}" \
+    -diffdir "${IMPOSM_DIFF_DIR:?}" \
+    -expiretiles-dir "${EXPIRETILES_DIR:?}" \
+    -expiretiles-zoom "${EXPIRETILES_ZOOM:-14}" \
+    -config "${IMPOSM_CONFIG_FILE:?}"

commit 0ab3a7060e54c7547e4a89b629027d3e75d032f2

Fix missing EXPIRETILES_DIR value
This was broken in bb072fee3725f4967bc68922bed19cb8442d3cad - #234

Dockerfile
@@ -84,6 +84,7 @@ ENV TOOLS_DIR="$TOOLS_DIR" \
     IMPOSM_MAPPING_FILE=/mapping/mapping.yaml \
     IMPOSM_CACHE_DIR=/cache \
     IMPOSM_DIFF_DIR=/import \
+    EXPIRETILES_DIR=/import \
     PBF_DATA_DIR=/import \
     SQL_DIR=/sql \
     SQL_TOOLS_DIR="${TOOLS_DIR}/sql"