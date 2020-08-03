fa0e557a41ffea79fe5426ee5e08490b94f1fb20
lazaa32 <ad.laza32@gmail.com>
2020-08-03 15:56:20 +0200

    Handle unbound vars.

--- docker/generate-vectortiles-pg/export-local-pgquery-pool.sh
+++ docker/generate-vectortiles-pg/export-local-pgquery-pool.sh
@@ -10,12 +10,12 @@ readonly MID_ZOOM=${MID_ZOOM:-12}
 readonly MAX_ZOOM=${MAX_ZOOM:-14}
 readonly BBOX=${BBOX:-"-180.0,-85.0511,180.0,85.0511"}
 
-PGHOSTS_LIST="host=10.220.44.57&host=10.220.44.56&host=10.220.44.59"  # &host=...&host=...
-PGPORT=5432
-HOST_COUNT=3  # number of postgres servers
-CPU=16  # number of CPUs per postgres server
-MAXCONNECTIONS=$(( CPU + CPU / 10 ))
-ALL_STREAMS=$(( MAXCONNECTIONS * HOST_COUNT ))
+readonly PGHOSTS_LIST=${PGHOSTS_LIST:-"postgres"} # "host=10.220.44.57&host=10.220.44.56&host=10.220.44.59"
+readonly PGPORT=${PGPORT:-5432}
+readonly HOST_COUNT=${HOST_COUNT:-3}  # number of postgres servers
+readonly CPU=${CPU:-16}  # number of CPUs per postgres server
+readonly MAXCONNECTIONS=$(( CPU + CPU / 10 ))
+readonly ALL_STREAMS=$(( MAXCONNECTIONS * HOST_COUNT ))
 
 PQGUERY="pgquery://?database=$PGDATABASE&$PGHOSTS_LIST&port=$PGPORT&username=$PGUSER&password=$PGPASSWORD&funcZXY=getmvt&testOnStartup=false&maxpool=$MAXCONNECTIONS&nogzip=1"
 echo $PQGUERY
