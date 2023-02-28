966c0a5dd34196659dcd490ac836e7f82e975a78
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2023-02-28 10:51:31 -0500

    Use more descriptive variable names

--- sql/LabelCenterline.sql
+++ sql/LabelCenterline.sql
@@ -9,12 +9,12 @@ __Parameters:__
 
 __Returns:__ `geometry(multiline)`
 ******************************************************************************/
-CREATE OR REPLACE FUNCTION CountDisconnectedEndpoints(polyline geometry, line geometry)
+CREATE OR REPLACE FUNCTION CountDisconnectedEndpoints(polyline geometry, testline geometry)
     RETURNS integer AS $$
     BEGIN
         RETURN ST_NPoints(ST_RemoveRepeatedPoints(ST_Points(polyline)))
-                        - ST_NPoints(ST_RemoveRepeatedPoints(ST_Points(ST_Difference(polyline, line))))
-                        - ST_NPoints(line) + 2;
+                    - ST_NPoints(ST_RemoveRepeatedPoints(ST_Points(ST_Difference(polyline, testline))))
+                    - ST_NPoints(testline) + 2;
     END
     $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION TrimmedCenterline(inPolyline geometry)
@@ -22,13 +22,13 @@ CREATE OR REPLACE FUNCTION TrimmedCenterline(inPolyline geometry)
     DECLARE outPolyline geometry;
     BEGIN
         WITH tbla AS (
-            SELECT inPolyline as polyline, (ST_Dump(inPolyline)).geom as line
+            SELECT inPolyline as polyline, (ST_Dump(inPolyline)).geom as edge
         ),
         tblb AS (
-            SELECT polyline, line as shortestBranchLine
+            SELECT polyline, edge as shortestBranchLine
             FROM tbla
-            WHERE CountDisconnectedEndpoints(polyline, line) > 0
-            ORDER BY ST_Length(line) ASC
+            WHERE CountDisconnectedEndpoints(polyline, edge) > 0
+            ORDER BY ST_Length(edge) ASC
             LIMIT 1
         ),
         tblc AS (
@@ -48,27 +48,24 @@ CREATE OR REPLACE FUNCTION LabelCenterline(inGeometry geometry)
     DECLARE outPolyline geometry;
     BEGIN
         WITH tbla AS (
-            SELECT inGeometry as polygon WHERE ST_GeometryType(inGeometry) = 'ST_Polygon'
+            SELECT inGeometry as inPolygon WHERE ST_GeometryType(inGeometry) = 'ST_Polygon'
             UNION ALL
-            SELECT ST_ConcaveHull(ST_Points(ST_Simplify(inGeometry, 25)), 0.2) as polygon WHERE ST_GeometryType(inGeometry)='ST_MultiPolygon'
+            SELECT ST_ConcaveHull(ST_Points(ST_Simplify(inGeometry, 25)), 0.2) as inPolygon WHERE ST_GeometryType(inGeometry)='ST_MultiPolygon'
         ),
         tblb AS (
-            SELECT ST_MakePolygon(ST_ExteriorRing(polygon)) as polygon
+            SELECT ST_MakePolygon(ST_ExteriorRing(inPolygon)) as shellPolygon
             FROM tbla
         ),
         tblc AS (
-            SELECT polygon, (ST_Dump(ST_VoronoiLines(ST_LineInterpolatePoints(ST_Boundary(polygon), 0.0075)))).geom as lines
+            SELECT shellPolygon, (ST_Dump(ST_VoronoiLines(ST_LineInterpolatePoints(ST_Boundary(shellPolygon), 0.0075)))).geom as voroniLines
             FROM tblb
         ),
         tbld AS (
-            SELECT ST_LineMerge(ST_Collect(lines)) as polyline
+            SELECT ST_LineMerge(ST_Collect(voroniLines)) as voroniPolyline
             FROM tblc
-            WHERE ST_Contains(polygon, lines)
+            WHERE ST_Contains(shellPolygon, voroniLines)
         )
-        SELECT ST_ChaikinSmoothing(ST_SimplifyPreserveTopology(TrimmedCenterline(polyline), 80), 3, false) INTO outPolyline FROM tbld;
+        SELECT ST_ChaikinSmoothing(ST_SimplifyPreserveTopology(TrimmedCenterline(voroniPolyline), 80), 3, false) INTO outPolyline FROM tbld;
         RETURN outPolyline;
     END
     $$ LANGUAGE plpgsql;
-
-
-
